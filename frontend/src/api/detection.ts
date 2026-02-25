89e94132abd9d55a332969a44bb00760dc9124b1
ezki3le <ys.dolle@gmail.com>
2026-02-25 15:59:12 +0100

    Ménage

--- frontend/src/api/detection.ts
+++
@@ -1,8 +0,0 @@
-import { api } from './client'
-import type { BlurState } from '@/types/api'
-
-export const fetchBlurState = () =>
-  api.get<BlurState>('/api/blur')
-
-export const toggleBlur = () =>
-  api.post('/api/blur/toggle')
--- frontend/src/api/videos.ts
+++
@@ -1,8 +0,0 @@
-import { api } from './client'
-import type { VideosResponse, VideoInfo } from '@/types/api'
-
-export const fetchVideos = () =>
-  api.get<VideosResponse>('/api/videos')
-
-export const fetchVideoInfo = (video: string) =>
-  api.get<VideoInfo>(`/api/videos/${encodeURIComponent(video)}/info`)
